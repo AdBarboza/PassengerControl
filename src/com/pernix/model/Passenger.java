b9f29eb88a678cdff0f3a9d54ab93ec697359b79
Adrian Barboza <adrian.b.p03@gmail.com>
2018-06-21 09:57:20 -0600

    Added Passenger and Flight clases

---
+++ src/com/pernix/model/Passenger.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pernix.model;
+
+/**
+ *
+ * @author Adrián
+ */
+public class Passenger {
+    
+}
